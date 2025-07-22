fca134bb8508dc024f90553d00d5115c4b4b6250
Student <Student@Train11-5-PC139.control.ad.sit.kmutt.ac.th>
2025-07-22 15:27:25 +0700

    Add class2

---
+++ class2.py
@@ -0,0 +1,68 @@
+# # # # # # # # age = 69
+# # # # # # # # print(age)
+
+# # # # # # # # num = 7
+# # # # # # # # print(type(num))
+
+# # # # # # # # num2 = 1.1
+# # # # # # # # print(type(num2))
+
+# # # # # # # # boolean = True
+# # # # # # # # print(type(boolean))
+
+# # # # # # # # name = "Pond 6969"
+# # # # # # # # print(type(name))
+# # # # # # # # print(name)
+
+# # # # # # # age = "19"
+# # # # # # # print(type(age))
+
+# # # # # # # age2 = int(age)
+# # # # # # # print(type(age2))
+
+# # # # # # # num1 = 1.1
+# # # # # # # print(type(num1))
+
+# # # # # # # num2 = float(num1)
+# # # # # # # print(type(num2))
+# # # # # # # print(num2)
+
+# # # # # # age = 19.5
+# # # # # # print(type(age))
+
+# # # # # # age2 = str(age)
+# # # # # # print(type(age2))
+
+
+# # # # # # tf = True
+# # # # # # print(type(tf))
+
+# # # # # # tf2 = str(tf)
+# # # # # # print(type(tf2))
+
+
+# # # # # sum = 
+# # # # # print(sum)
+# # # # # print(type(sum))
+
+# # # # x = "hello"
+# # # # y = "world"
+# # # # print(x, y)
+
+# # # age = int(input("Enter your age."))
+# # # print("Your age is", age)
+# # # print(type(age))
+
+# # name = str(input("What's your name. "))
+# # print("Hello!", name)
+
+# age = int(input("What's your age? \n"))
+# if age <= 18:
+#     print("Ptak Phu Yao")
+# elif 19 <= age <= 99:
+#     print("Uheh uheh")
+# else:
+#     print("Khee Moe Wa")
+
+
+
